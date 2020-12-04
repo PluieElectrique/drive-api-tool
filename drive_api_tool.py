b4736f0bf3b2e400dda03fa0463ef0be09077db6
PluieElectrique <41453973+PluieElectrique@users.noreply.github.com>
2020-12-04 04:41:56 -0800

    Re-enable validation

--- drive_api_tool.py
+++ drive_api_tool.py
@@ -129,9 +129,7 @@ async def get_metadata(user_creds, ids, fields, max_concurrent, quota):
     error_counts = defaultdict(int)
     pbar = tqdm(total=len(ids), unit="req")
     async with Aiogoogle(user_creds=user_creds) as aiogoogle:
-        # TODO: Disable validation to work around a typo. Remove when
-        # https://github.com/omarryhan/aiogoogle/pull/35 is merged.
-        drive = await aiogoogle.discover("drive", "v3", validate=False)
+        drive = await aiogoogle.discover("drive", "v3")
         coros = [
             aiogoogle.as_user(drive.files.get(fileId=id, fields=fields)) for id in ids
         ]
