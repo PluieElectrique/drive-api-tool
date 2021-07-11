bc69846914fd2ec39f0c95a0e69f5933472740c0
PluieElectrique <41453973+PluieElectrique@users.noreply.github.com>
2021-07-11 01:51:20 -0700

    Don't try to extract IDs from request URLs
    It's too hard to do for every kind of request. And, it can be useful to
    know exactly what the request URL was. Though it does increase the
    length of the output.

--- util.py
+++ util.py
@@ -12,11 +12,7 @@ class ErrorTracker:
         try:
             return await coro
         except Exception as exc:
-            error = {
-                # Neither the request nor the response provides the file ID, so
-                # we have to extract it ourselves.
-                "id": exc.req.url.split("/")[-1].split("?")[0],
-            }
+            error = {"url": exc.req.url}
             if exc.res.json is not None and "error" in exc.res.json:
                 json_error = exc.res.json["error"]
                 error.update(
